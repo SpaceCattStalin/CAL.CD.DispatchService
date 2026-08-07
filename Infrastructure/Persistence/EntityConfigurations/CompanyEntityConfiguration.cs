991f52cba5bcc2b68657279b38c850bc4367cc40
SpaceCattStalin <spacestalincyka@gmail.com>
2026-08-07 09:51:19 +0700

    chore: update Dispatch, DispatchDriver entities

--- Infrastructure/Persistence/EntityConfigurations/CompanyEntityConfiguration.cs
+++ Infrastructure/Persistence/EntityConfigurations/CompanyEntityConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Domain;
 
@@ -8,13 +8,18 @@ public class CompanyEntityConfiguration : IEntityTypeConfiguration<Company>
 {
     public void Configure(EntityTypeBuilder<Company> builder)
     {
-        builder.ToTable("companies");
+        builder.ToTable("companies", t =>
+        {
+            t.HasCheckConstraint("CK_companies_company_name_min_length", "LENGTH(company_name) >= 5");
+            t.HasCheckConstraint("CK_companies_company_phone_min_length", "LENGTH(company_phone) >= 10");
+            t.HasCheckConstraint("CK_companies_company_email_min_length", "LENGTH(company_email) >= 15");
+        });
 
         builder.HasKey(x => x.CompanyId);
 
-        builder.Property(x => x.CompanyName).HasColumnName("company_name");
-        builder.Property(x => x.CompanyPhone).HasColumnName("company_phone");
-        builder.Property(x => x.CompanyEmail).HasColumnName("company_email");
+        builder.Property(x => x.CompanyName).HasColumnName("company_name").HasMaxLength(50);
+        builder.Property(x => x.CompanyPhone).HasColumnName("company_phone").HasMaxLength(12);
+        builder.Property(x => x.CompanyEmail).HasColumnName("company_email").HasMaxLength(30);
         builder.Property(x => x.CompanyType).HasColumnName("type").HasConversion<string>();
         builder.Property(x => x.RecordVersion).HasColumnName("record_version").IsRowVersion();
         builder.Property(x => x.CreatedAt).HasColumnName("created_at");
