a67212ac0124da04f81605304ca5bb5a14c7040a
SpaceCattStalin <spacestalincyka@gmail.com>
2026-08-18 16:06:22 +0700

    refactor: move registered classes to Extensions folder

---
+++ Presentation/Extensions/AuthConfigurationExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using Application.Auth;
+using Application.Interfaces;
+using Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Presentation.Services;
+
+namespace Presentation;
+
+public static class AuthConfigurationExtensions
+{
+    public static IServiceCollection AddAuthenticationAndAuthorizeConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<AuthService>();
+        services.AddScoped<ICurrentUserService, CurrentUserService>();
+        services.AddScoped<IPasswordHasher, Pbkdf2PasswordHasher>();
+
+        services.Configure<JwtSettings>(configuration.GetSection("Jwt"));
+        services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        var jwtSettings = configuration.GetSection("Jwt").Get<JwtSettings>()
+            ?? throw new InvalidOperationException("Jwt settings were not found.");
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = jwtSettings.Audience,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SigningKey)),
+                    ValidateLifetime = true
+                };
+            });
+
+        // Must register an instance (not the type) here: PermissionAuthorizationRequirement's constructor
+        // takes params string[] allowedPermissions, which DI can't resolve on its own.
+        services.AddSingleton<IAuthorizationHandler>(new PermissionAuthorizationRequirement());
+
+        services.AddAuthorization(options =>
+        {
+            options.FallbackPolicy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+
+            foreach (var permission in PermissionNames.All)
+            {
+                options.AddPolicy(permission, policy =>
+                    policy.Requirements.Add(new PermissionAuthorizationRequirement(permission)));
+            }
+        });
+
+        return services;
+    }
+}
---
+++ Presentation/Extensions/ValidatorConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Application.Auth;
+using Application.Dispatches;
+using Application.Dispatches.Validator;
+using FluentValidation;
+
+namespace Presentation;
+
+public static class ValidatorConfigurationExtensions
+{
+    public static IServiceCollection AddValidatorConfiguration(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<CreateDispatchRequest>, CreateDispatchRequestValidator>();
+        services.AddScoped<IValidator<GetDispatchBatchRequest>, GetDispatchBatchRequestValidator>();
+        services.AddScoped<IValidator<AssignDriverRequest>, AssignDriverRequestValidator>();
+        services.AddScoped<IValidator<UpdateDispatchRequest>, UpdateDispatchRequestValidator>();
+        services.AddScoped<IValidator<LoginRequest>, LoginRequestValidator>();
+
+        return services;
+    }
+}
