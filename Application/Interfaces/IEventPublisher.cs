adca5f1f6bf892d4c9bc74e51096f2c12e3d5b56
SpaceCattStalin <spacestalincyka@gmail.com>
2026-08-22 13:50:02 +0700

    feat: publish dispatch update events to SNS on update

--- Application/Interfaces/IEventPublisher.cs
+++ Application/Interfaces/IEventPublisher.cs
@@ -6,4 +6,5 @@ public interface IEventPublisher
 {
     public Task Publish(DispatchWriterEvent writerEvent);
     public Task Publish(DispatchDeleteEvent deleteEvent);
+    public Task Publish(DispatchUpdateEvent updateEvent);
 }
